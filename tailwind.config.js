808d2cf0f196c34b7a2570a71f81a15a3e905e3f
Milan Solanki <milan.solanki@kevit.io>
2023-03-21 18:18:26 +0530

    completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,ts}"],
   theme: {
     extend: {},
     container: {
@@ -11,6 +11,15 @@ module.exports = {
         lg: '5rem',
         xl: '6rem',
         '2xl': '7rem',
+      },
+      fontSize: {
+        sm: '0.8rem',
+        base: '1rem',
+        xl: '1.25rem',
+        '2xl': '1.563rem',
+        '3xl': '1.953rem',
+        '4xl': '2.441rem',
+        '5xl': '3.052rem',
       },
     },
   },
